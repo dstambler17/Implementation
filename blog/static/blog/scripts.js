b1e333953828f2dce797c7e0f6beea7096aab64f
Lev Stambler <levstamb@gmail.com>
2019-07-18 09:13:19 -0400

    styling and trying to get music to work

--- blog/static/blog/scripts.js
+++ blog/static/blog/scripts.js
@@ -1,23 +1,23 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".is-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".is-fixed-top");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 
-    var $head = $(".headingText");
-    $head.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});
+//     var $head = $(".headingText");
+//     $head.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//   });
+// });
 
 
-//Make navbar supercool for mobile
-$(document).ready(function() {
+// //Make navbar supercool for mobile
+// $(document).ready(function() {
 
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+//   // Check for click events on the navbar burger icon
+//   $(".navbar-burger").click(function() {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+//       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//       $(".navbar-burger").toggleClass("is-active");
+//       $(".navbar-menu").toggleClass("is-active");
 
-  });
-});
+//   });
+// });
